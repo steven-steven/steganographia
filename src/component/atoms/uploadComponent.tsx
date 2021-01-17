92b5a407fe3b23f8788c322d20587ab7c94b43ff
Steven Xiong <ssxiong@uwaterloo.ca>
2021-01-17 02:55:02 -0500

    Refactored submission form and added error checking.

--- src/component/atoms/uploadComponent.tsx
+++ src/component/atoms/uploadComponent.tsx
@@ -1,52 +1,18 @@
 import React from "react";
-import axios from "axios";
 import _ from "lodash";
 
 /**
  * Credits go to https://maxcode.online/nextjs-file-upload-with-uppy-and-multer-using-api-routes/
  */
-const UploadComponent = ({ handleUpload, fileData }) => {
-  const handleFileSubmit = (e) => {
-    e.preventDefault();
-
-    let formData = new FormData();
-
-    formData.append("myImage", fileData);
-    formData.append("name", "Steven Xiong");
-    formData.append("email", "steven.x@waterloop.ca");
-
-    axios({
-      method: "post",
-      url: "/api/incidents/upload",
-      data: formData,
-      headers: { "Content-Type": "multipart/form-data" },
-    })
-      .then(function (response) {
-        console.log(response.status);
-      })
-      .catch(function (response) {
-        console.log(response.status);
-      });
-  };
-
+const UploadComponent = ({ handleUpload }) => {
   return (
-    <div className="mt-6 flex justify-between">
-      <div>
-        <input
-          type="file"
-          name="myImage"
-          accept="image/*"
-          onChange={handleUpload}
-        />
-      </div>
-      <div>
-        <input
-          type="submit"
-          value="Submit"
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          onClick={handleFileSubmit}
-        />
-      </div>
+    <div>
+      <input
+        type="file"
+        name="myImage"
+        accept="image/*"
+        onChange={handleUpload}
+      />
     </div>
   );
 };
--- src/component/molecules/formComponent.tsx
+++ src/component/molecules/formComponent.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+
+import axios from "axios";
 import UploadComponent from "../atoms/uploadComponent";
 
 const FormComponent = () => {
@@ -8,8 +10,47 @@ const FormComponent = () => {
     uploadedImage: "",
   });
 
+  const hasNoErrors = () => {
+    if (!formData.name) {
+      alert("Please enter your name");
+      return false;
+    }
+    if (!formData.email) {
+      alert("Please enter your email");
+      return false;
+    }
+    if (!formData.uploadedImage) {
+      alert("Please upload an image");
+      return false;
+    }
+    return true;
+  };
+
   const handleUpload = (file) => {
     setFormData({ ...formData, uploadedImage: file });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (hasNoErrors() === false) return;
+
+    let form = new FormData();
+    form.append("myImage", formData.uploadedImage);
+    form.append("name", formData.name);
+    form.append("email", formData.email);
+
+    axios({
+      method: "post",
+      url: "/api/incidents/upload",
+      data: form,
+      headers: { "Content-Type": "multipart/form-data" },
+    })
+      .then(function (response) {
+        console.log(response.status);
+      })
+      .catch(function (response) {
+        console.log(response.status);
+      });
   };
 
   return (
@@ -24,15 +65,14 @@ const FormComponent = () => {
             name="name"
             type="name"
             value={formData.name}
-            onChange={(e) =>
-              setFormData({ ...formData, email: e.target.value })
-            }
+            onChange={(e) => setFormData({ ...formData, name: e.target.value })}
             autoComplete="name"
             required
             className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
             placeholder="Name"
           />
         </div>
+
         <div>
           <label htmlFor="email-address" className="sr-only">
             Email address
@@ -52,10 +92,20 @@ const FormComponent = () => {
           />
         </div>
       </div>
-      <UploadComponent
-        handleUpload={(e) => handleUpload(e.target.files[0])}
-        fileData={formData.uploadedImage}
-      />
+
+      <div className="mt-6 flex justify-between">
+        <UploadComponent
+          handleUpload={(e) => handleUpload(e.target.files[0])}
+        />
+        <div>
+          <input
+            type="submit"
+            value="Submit"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            onClick={handleSubmit}
+          />
+        </div>
+      </div>
     </form>
   );
 };
