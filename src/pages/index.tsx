330e0e11981443e637371ce92fe62b5e2d61f9a0
Steven Xiong <ssxiong@uwaterloo.ca>
2021-01-17 03:48:27 -0500

    Added website description to home page.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,6 +19,14 @@ export default function Home() {
           <h1 className="px-5 mb-8 text-4xl font-bold leading-tight tracking-tight text-center sm:mt-4 sm:text-6xl">
             Steganographia
           </h1>
+          <h4 className="text-center sm:mt-4">
+            We help image authors and meme creators claim ownership over their
+            work.
+          </h4>
+          <h4 className="mb-8 text-center">
+            Please specify your name and upload an image to claim ownership over
+            it.
+          </h4>
           <FormComponent />
         </div>
       </main>
