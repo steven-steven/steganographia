4b0b3d28e0721263ce3e4213fd52a260d8f03f14
Steven <steven.infinity29@gmail.com>
2021-01-17 00:49:18 -0500

    Implement DB call to retrieve and store data

--- src/handler/dbHandler.tsx
+++ src/handler/dbHandler.tsx
@@ -3,55 +3,47 @@ const CONFIG = require('../config/dbConfig.tsx');
 // Create a pool.
 const pool = new pg.Pool(CONFIG);
 
-new Promise((resolve, reject) => {
-    pool.query(`DELETE FROM my_table WHERE uid=123;`)
+module.exports = {
+  selectAll: () => {
+    return new Promise((resolve, reject) => {
+      pool.query('select * from imagemeta;')
+        .then(res => resolve(res.rows))
+        .catch(err =>
+          reject(err)
+        );
+    });
+  },
+  selectOne: (uuid) => {
+    return new Promise((resolve, reject) => {
+      pool.query(`SELECT * FROM imagemeta WHERE uid='${uuid}';`)
+        .then(res => resolve(res.rows))
+        .catch(err =>
+          reject(err)
+        );
+    });
+  },
+  insert: (uuid, name, message, date) => {
+    console.log(`INSERT INTO imagemeta VALUES ('${uuid}', '${name}', '${message}', '${date.toString()}');`);
+    return new Promise((resolve, reject) => {
+      pool.query(`INSERT INTO imagemeta VALUES ('${uuid}', '${name}', '${message}', '${date.toString()}');`)
+        .then(res => resolve(res))
+        .catch(err => {
+          console.log(err);
+          reject(err)
+        }
+        );
+    });
+  },
+  delete: (uuid) => {
+    return new Promise((resolve, reject) => {
+      pool.query(`DELETE FROM imagemeta WHERE uid='${uuid}';`)
         .then(res => resolve(res))
         .catch(err =>
-            reject(err)
+          reject(err)
         );
-}).then((res) => {
-    console.log(res);
-})
-pool.end();
-
-module.exports = {
-    selectAll: () => {
-        return new Promise((resolve, reject) => {
-            pool.query('select * from my_table;')
-                .then(res => resolve(res.rows))
-                .catch(err =>
-                    reject(err)
-                );
-        });
-    },
-    selectOne: (uuid) => {
-        return new Promise((resolve, reject) => {
-            pool.query(`SELECT * FROM my_table WHERE uid=${uuid};`)
-                .then(res => resolve(res.rows))
-                .catch(err =>
-                    reject(err)
-                );
-        });
-    },
-    insert: (uuid, name, message, date) => {
-        return new Promise((resolve, reject) => {
-            pool.query(`INSERT INTO my_table VALUES (${uuid}, ${name}, ${message}, ${date.toString()});`)
-                .then(res => resolve(res))
-                .catch(err =>
-                    reject(err)
-                );
-        });
-    },
-    delete: (uuid) => {
-        return new Promise((resolve, reject) => {
-            pool.query(`DELETE FROM my_table WHERE uid=${uuid};`)
-                .then(res => resolve(res))
-                .catch(err =>
-                    reject(err)
-                );
-        });
-    },
-    shutdown: () => {
-        pool.end();
-    }
+    });
+  },
+  shutdown: () => {
+    pool.end();
+  }
 }
